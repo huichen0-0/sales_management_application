953cc6b7734961b74c28f2cd978f5622ef89836d
Huichen <huyquangnguyeen@gmail.com>
2024-10-28 16:50:14 +0700

    first commit

---
+++ android/app/src/main/java/com/example/sales_management_application/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.sales_management_application;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/example/sales_management_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sales_management_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
