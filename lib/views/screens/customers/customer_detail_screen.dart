f3365a42988c8e9ddb311bc612f3609954a438e8
Huichen <huyquangnguyeen@gmail.com>
2024-11-17 12:11:52 +0700

    customer page, fix widgets page

--- lib/views/screens/customers/customer_detail_screen.dart
+++ lib/views/screens/customers/customer_detail_screen.dart
@@ -1,10 +1,332 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomerDetailScreen extends StatelessWidget {
+class CustomerDetailScreen extends StatefulWidget {
   const CustomerDetailScreen({super.key});
 
   @override
+  State<CustomerDetailScreen> createState() => _CustomerDetailScreenState();
+}
+
+class _CustomerDetailScreenState extends State<CustomerDetailScreen> {
+  bool isActive =
+  true; // biến check trạng thái hoạt động TODO: lấy từ thuộc tính của ncc
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ///fake dữ liệu
+    final Map<String, dynamic> supplier = {
+      'id': 1,
+      'name': 'ABC1',
+      'phone': '0987654321',
+      'sex': 'Nam',
+      'amount': 1000000,
+      'address': '123 Đường ABC, Quận 1, TP.HCM',
+      'email': 'kh@example.com',
+      'notes': 'KH vip',
+    };
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Chi tiết khách hàng'),
+        actions: [
+          ///nút sửa
+          IconButton(
+            onPressed: () {
+              context.push('/customers/:id/edit');
+            },
+            icon: const Icon(Icons.edit),
+          ),
+
+          ///nút xóa
+          IconButton(
+            onPressed: () {
+              _showDeleteCustomerDialog();
+            },
+            icon: const Icon(Icons.delete),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Tên khách hàng
+              ListTile(
+                title: const Text(
+                  'Tên khách hàng',
+                ),
+                trailing: Text(
+                  supplier['name'],
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(),
+
+              /// Điện thoại TODO: xử lý gọi khi click vào
+              ListTile(
+                title: const Text(
+                  'Số điện thoại',
+                ),
+                trailing: TextButton.icon(
+                  label: const Text(
+                    '0987654321',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  icon: const Icon(
+                    Icons.phone_enabled,
+                    size: 14,
+                    color: Colors.blue,
+                  ),
+                  iconAlignment: IconAlignment.end,
+                  onPressed: () {},
+                ),
+              ),
+              const Divider(),
+              /// Giới tính
+              ListTile(
+                title: const Text(
+                  'Giới tính',
+                ),
+                trailing: Text(
+                  supplier['sex'],
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(),
+
+              /// Lịch sử giao dịch và Công nợ TODO: xử lý xem chi tiết
+              ListTile(
+                title: const Text('Lịch sử giao dịch'),
+                trailing: TextButton.icon(
+                  label: const Text(
+                    '0',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                    color: Colors.blue,
+                  ),
+                  iconAlignment: IconAlignment.end,
+                  onPressed: () {},
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                title: const Text('Công nợ'),
+                trailing: TextButton.icon(
+                  label: const Text(
+                    '0',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                    color: Colors.blue,
+                  ),
+                  iconAlignment: IconAlignment.end,
+                  onPressed: () {},
+                ),
+              ),
+              const Divider(),
+
+              /// Địa chỉ
+              ListTile(
+                title: const Text(
+                  'Địa chỉ',
+                ),
+                trailing: Text(
+                  supplier['address'],
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(),
+
+              /// Email
+              ListTile(
+                title: const Text(
+                  'Email',
+                ),
+                trailing: Text(
+                  supplier['email'],
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(),
+
+              /// Ghi chú
+              ListTile(
+                title: const Text(
+                  'Ghi chú',
+                ),
+                trailing: Text(
+                  supplier['notes'],
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(),
+
+              /// trạng thái hoạt động TODO: xử lý bật tắt hoạt động
+              ListTile(
+                title: const Text(
+                  'Trạng thái hoạt động',
+                  style: TextStyle(color: Colors.black),
+                ),
+                trailing: Switch(
+                  value: isActive,
+                  onChanged: (bool value) {
+                    _showConfirmationDialog(value);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Hàm hiển thị hộp thoại xác nhận xóa khách hàng
+  void _showDeleteCustomerDialog() {
+    // Hiển thị hộp thoại xác nhận
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.warning_amber,
+                color: Colors.red,
+                size: 60,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Bạn chắc chắn muốn xóa khách hàng này?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            /// Nút Hủy
+            IconButton(
+              icon: const Icon(
+                Icons.cancel_outlined,
+                color: Colors.red,
+                size: 35,
+              ),
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(); // Đóng hộp thoại mà không thay đổi gì
+              },
+            ),
+
+            /// Nút Đồng ý
+            IconButton(
+              icon: const Icon(
+                Icons.check,
+                color: Colors.green,
+                size: 35,
+              ),
+              onPressed: () {
+                //TODO: thực hiện thao tác xóa
+                context.go('/customers');
+              },
+            ),
+          ],
+          actionsAlignment: MainAxisAlignment.spaceEvenly,
+        );
+      },
+    );
+  }
+
+  /// Hàm hiển thị hộp thoại xác nhận khi thay đổi trạng thái
+  void _showConfirmationDialog(bool newValue) {
+    // Nội dung thông báo tùy thuộc vào trạng thái mới
+    String dialogMessage = newValue
+        ? 'Bật hoạt động của khách hàng này?'
+        : 'Tắt hoạt động của khách hàng này?';
+
+    // Hiển thị hộp thoại xác nhận
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.warning_amber,
+                color: Colors.red,
+                size: 60,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                dialogMessage,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            /// Nút Hủy (không thay đổi trạng thái)
+            IconButton(
+              icon: const Icon(
+                Icons.cancel_outlined,
+                color: Colors.red,
+                size: 35,
+              ),
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(); // Đóng hộp thoại mà không thay đổi gì
+              },
+            ),
+
+            /// Nút Đồng ý (thay đổi trạng thái) TODO: cập nhật trạng thái trong db
+            IconButton(
+              icon: const Icon(
+                Icons.check,
+                color: Colors.green,
+                size: 35,
+              ),
+              onPressed: () {
+                setState(() {
+                  // Cập nhật trạng thái dựa trên lựa chọn của người dùng
+                  isActive = newValue;
+                });
+                Navigator.of(context).pop(); // Đóng hộp thoại
+              },
+            ),
+          ],
+          actionsAlignment: MainAxisAlignment.spaceEvenly,
+        );
+      },
+    );
   }
 }
+
--- lib/views/screens/home/widgets_screen.dart
+++ lib/views/screens/home/widgets_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../widgets/custom_card.dart';
 
 class WidgetScreen extends StatelessWidget {
@@ -9,68 +10,265 @@ class WidgetScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.all(16.0),
-        children: const [
-          WidgetCard(
-            title: 'Tài khoản',
-            items: [
-              {'icon': Icons.person, 'label': 'Thông tin', 'link': '/home'},
-              {'icon': Icons.lock, 'label': 'Đổi mật khẩu', 'link': '/reset_password'},
-              {'icon': Icons.logout, 'label': 'Đăng xuất', 'link': '/'},
+        children: [
+          _buildAccountInfoCard(
+            context,
+            [
+              {
+                'icon': Icons.lock,
+                'label': 'Đổi mật khẩu',
+                'action': () {
+                  context.go('/reset_password');
+                },
+              },
+              {
+                'icon': Icons.logout,
+                'label': 'Đăng xuất',
+                'action': () {
+                  context.go('/sell');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Giao dịch',
             items: [
-              {'icon': Icons.shopping_bag, 'label': 'Bán hàng', 'link': '/order'},
-              {'icon': Icons.receipt, 'label': 'Hóa đơn', 'link': '/invoice'},
-              {'icon': Icons.add_shopping_cart, 'label': 'Đặt hàng', 'link': '/order'},
-              {'icon': Icons.assignment_return, 'label': 'Trả hàng', 'link': '/home'},
+              {
+                'icon': Icons.shopping_bag,
+                'label': 'Bán hàng',
+                'action': () {
+                  context.go('/sell');
+                },
+              },
+              {
+                'icon': Icons.receipt,
+                'label': 'Hóa đơn',
+                'action': () {
+                  context.go('/invoices');
+                },
+              },
+              {
+                'icon': Icons.add_shopping_cart,
+                'label': 'Đặt hàng',
+                'action': () {
+                  context.go('/orders');
+                },
+              },
+              {
+                'icon': Icons.assignment_return,
+                'label': 'Trả hàng',
+                'action': () {
+                  context.go('/home');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Hàng hóa',
             items: [
-              {'icon': Icons.inventory, 'label': 'Hàng hóa', 'link': '/product'},
-              {'icon': Icons.check_box, 'label': 'Kiểm kho', 'link': '/home'},
-              {'icon': Icons.input, 'label': 'Nhập hàng', 'link': '/home'},
-              {'icon': Icons.assignment_return, 'label': 'Trả hàng nhập', 'link': '/home'},
-              {'icon': Icons.cancel, 'label': 'Xuất hủy', 'link': '/home'},
+              {
+                'icon': Icons.inventory,
+                'label': 'Hàng hóa',
+                'action': () {
+                  context.go('/products');
+                },
+              },
+              {
+                'icon': Icons.check_box,
+                'label': 'Kiểm kho',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.input,
+                'label': 'Nhập hàng',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.assignment_return,
+                'label': 'Trả hàng nhập',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.cancel,
+                'label': 'Xuất hủy',
+                'action': () {
+                  context.go('/home');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Đối tác',
             items: [
-              {'icon': Icons.person, 'label': 'Khách hàng', 'link': '/customers'},
-              {'icon': Icons.group, 'label': 'Nhà cung cấp', 'link': '/suppliers'},
+              {
+                'icon': Icons.person,
+                'label': 'Khách hàng',
+                'action': () {
+                  context.go('/customers');
+                },
+              },
+              {
+                'icon': Icons.group,
+                'label': 'Nhà cung cấp',
+                'action': () {
+                  context.go('/suppliers');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Báo cáo',
             items: [
-              {'icon': Icons.insert_chart_outlined, 'label': 'Cuối ngày', 'link': '/home'},
-              {'icon': Icons.inventory_outlined, 'label': 'Hàng hóa', 'link': '/home'},
-              {'icon': Icons.area_chart, 'label': 'Bán hàng', 'link': '/home'},
+              {
+                'icon': Icons.insert_chart_outlined,
+                'label': 'Cuối ngày',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.inventory_outlined,
+                'label': 'Hàng hóa',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.area_chart,
+                'label': 'Bán hàng',
+                'action': () {
+                  context.go('/home');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Cài đặt',
             items: [
-              {'icon': Icons.inventory, 'label': 'Hàng hóa', 'link': '/product'},
-              {'icon': Icons.sell, 'label': 'Bán hàng', 'link': '/home'},
-              {'icon': Icons.group, 'label': 'Đối tác', 'link': '/home'},
-              {'icon': Icons.devices_other, 'label': 'Ứng dụng & thiết bị', 'link': '/home'},
+              {
+                'icon': Icons.inventory,
+                'label': 'Hàng hóa',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.sell,
+                'label': 'Bán hàng',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.group,
+                'label': 'Đối tác',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.devices_other,
+                'label': 'Ứng dụng & thiết bị',
+                'action': () {
+                  context.go('/home');
+                },
+              },
             ],
           ),
           WidgetCard(
             title: 'Về chúng tôi',
             items: [
-              {'icon': Icons.menu_book, 'label': 'Hướng dẫn sử dụng', 'link': '/product'},
-              {'icon': Icons.event_note_outlined, 'label': 'Điều khoản sử dụng', 'link': '/tac'},
-              {'icon': Icons.phone, 'label': 'Tổng đài 1900 0091', 'link': '/home'},
+              {
+                'icon': Icons.menu_book,
+                'label': 'Hướng dẫn sử dụng',
+                'action': () {
+                  context.go('/home');
+                },
+              },
+              {
+                'icon': Icons.event_note_outlined,
+                'label': 'Điều khoản sử dụng',
+                'action': () {
+                  context.go('/tac');
+                },
+              },
+              {
+                'icon': Icons.phone,
+                'label': 'Tổng đài 1900 0091',
+                'action': () {
+                  context.go('/home');
+                },
+              },
             ],
           ),
         ],
       ),
     );
   }
+  Widget _buildAccountInfoCard(
+      BuildContext context, List<Map<String, dynamic>> items) {
+    return Card(
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Tài khoản',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'LayTenDeHienThi',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                Text('LayEmailDeHienThi', style: TextStyle(fontSize: 14)),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Wrap(
+              spacing: 16,
+              runSpacing: 16,
+              children: items.map((item) {
+                return TextButton(
+                  onPressed: () {
+                    item['action'];
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        item['icon'],
+                        size: 32,
+                        color: Colors.lightBlue,
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        item['label'],
+                        style: const TextStyle(color: Colors.lightBlue),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+
